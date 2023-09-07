1fca61626738549ec80ee7b74e28d5913697055a
dataentry <raghavrajpurohit81@gmail.com>
2023-09-07 18:29:17 +0530

    dialoge completed

--- lib/presentation/widgets/dialoge.dart
+++ lib/presentation/widgets/dialoge.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:new_marketing_dash_board/constants/colors.dart';
 import 'package:new_marketing_dash_board/constants/responsive.dart';
 import 'package:new_marketing_dash_board/presentation/widgets/myTextWidget.dart';
-import 'package:timelines/timelines.dart';
 
 class MyDialogesDetails extends StatefulWidget {
   static const String pageId = "/details";
@@ -13,7 +12,7 @@ class MyDialogesDetails extends StatefulWidget {
 }
 
 class _MyDialogesDetailsState extends State<MyDialogesDetails> {
-  PageController controller = PageController(initialPage: 2);
+  PageController controller = PageController(initialPage: 3);
   int _curr = 0;
   @override
   Widget build(BuildContext context) {
@@ -163,8 +162,81 @@ class _MyDialogesDetailsState extends State<MyDialogesDetails> {
   List<Widget> _getList(BuildContext ctx) => <Widget>[
         detailsContainer(ctx),
         documentsContainer(ctx),
-        permissionContainer(ctx)
+        permissionContainer(ctx),
+        finalContainer(ctx),
       ];
+  Widget finalContainer(context) {
+    return SizedBox(
+      height: double.infinity,
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: !(ResponsiveD.d(context) == ResponsiveD.mobile) ? 16 : 8,
+          right: !(ResponsiveD.d(context) == ResponsiveD.mobile) ? 16 : 8,
+          top: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const MyTextWidget(
+              content: "Form Status",
+              fontSize: 14,
+              fontbold: FontWeight.bold,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Wrap(
+              spacing: ResponsiveD.d(context) == ResponsiveD.desktop ? 20 : 10,
+              crossAxisAlignment: WrapCrossAlignment.start,
+              children: [
+                FilledButton.tonal(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    side: MaterialStateProperty.all(BorderSide(
+                        color: MyColor.blackText.withOpacity(0.30), width: 2)),
+                    backgroundColor:
+                        MaterialStateProperty.all(MyColor.whiteContainer),
+                  ),
+                  child: MyTextWidget(
+                    content: "Hired",
+                    myColor: MyColor.greyText,
+                  ),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    side: MaterialStateProperty.all(BorderSide(
+                        color: MyColor.blackText.withOpacity(0.30), width: 2)),
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
+                  ),
+                  child: MyTextWidget(
+                    content: "Pending",
+                    myColor: MyColor.greyText,
+                  ),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    side: MaterialStateProperty.all(BorderSide(
+                        color: MyColor.blackText.withOpacity(0.30), width: 2)),
+                    backgroundColor:
+                        MaterialStateProperty.all(MyColor.whiteContainer),
+                  ),
+                  child: MyTextWidget(
+                    content: "Rajected",
+                    myColor: MyColor.greyText,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   Widget detailsContainer(context) {
     return SizedBox(
@@ -670,58 +742,410 @@ class _MyDialogesDetailsState extends State<MyDialogesDetails> {
   }
 
   allChannelsWidget() {
-    return const SizedBox();
+    return Expanded(
+        child: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: MyColor.borderColor),
+            borderRadius: BorderRadius.circular(5)),
+        child: Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Column(children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                    columnSpacing:
+                        (ResponsiveD.d(context) == ResponsiveD.mobile)
+                            ? 100
+                            : (ResponsiveD.d(context) == ResponsiveD.tablet)
+                                ? 150
+                                : 250,
+                    showCheckboxColumn: true,
+                    columns: [
+                      DataColumn(
+                          label: MyTextWidget(
+                              content: "CHANNELS",
+                              myColor: MyColor.greyText,
+                              fontbold: FontWeight.bold)),
+                      DataColumn(
+                          label: MyTextWidget(
+                              content: "ACTION",
+                              myColor: MyColor.greyText,
+                              fontbold: FontWeight.bold)),
+                    ],
+                    rows: [
+                      for (Map m in tabelList)
+                        DataRow(
+                          cells: [
+                            DataCell(Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 20,
+                                  backgroundImage: NetworkImage(m["logo"]),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                InkWell(
+                                  child: MyTextWidget(
+                                    content: m["name"],
+                                    myColor: MyColor.blackText,
+                                    fontbold: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                  onTap: () {},
+                                ),
+                              ],
+                            )),
+                            DataCell(IconButton(
+                              icon: const Icon(Icons.add_box_outlined),
+                              onPressed: () {},
+                            )),
+                          ],
+                        )
+                    ]),
+              ),
+            ])),
+      ),
+    ));
   }
 
   specificChannels() {
     return Expanded(
-      child: SizedBox(
-        width: double.infinity,
-        child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: MyColor.borderColor),
-              borderRadius: BorderRadius.circular(5)),
-          child: Padding(
+        child: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: MyColor.borderColor),
+            borderRadius: BorderRadius.circular(5)),
+        child: Padding(
             padding: const EdgeInsets.only(top: 8),
             child: Column(children: [
               ElevatedButton(
                 style: ButtonStyle(elevation: MaterialStateProperty.all(1)),
                 child: const MyTextWidget(content: "Choose Channel"),
-                onPressed: () {},
+                onPressed: () {
+                  showDialog(
+                      builder: (context) => insideAllChannelsWidget(),
+                      context: context);
+                },
               ),
               const Divider(),
-              Padding(
-                padding: EdgeInsets.only(
-                    right: !(ResponsiveD.d(context) == ResponsiveD.mobile)
-                        ? 26
-                        : 0,
-                    left: !(ResponsiveD.d(context) == ResponsiveD.mobile)
-                        ? 26
-                        : 0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        MyTextWidget(
-                          fontSize: 12,
-                          content: "SELECTED CHANNELS (9)",
-                          myColor: MyColor.greyText,
-                        ),
-                        MyTextWidget(
-                          fontSize: 12,
-                          content: "ACTION",
-                          myColor: MyColor.greyText,
-                        ),
-                      ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                    columnSpacing:
+                        (ResponsiveD.d(context) == ResponsiveD.mobile)
+                            ? 100
+                            : (ResponsiveD.d(context) == ResponsiveD.tablet)
+                                ? 150
+                                : 250,
+                    showCheckboxColumn: true,
+                    columns: [
+                      DataColumn(
+                          label: MyTextWidget(
+                              content: "SELECTED CHANNELS (9)",
+                              myColor: MyColor.greyText,
+                              fontbold: FontWeight.bold)),
+                      DataColumn(
+                          label: MyTextWidget(
+                              content: "ACTION",
+                              myColor: MyColor.greyText,
+                              fontbold: FontWeight.bold)),
+                    ],
+                    rows: [
+                      for (Map m in tabelList)
+                        DataRow(
+                          cells: [
+                            DataCell(Row(
+                              children: [
+                                CircleAvatar(
+                                  radius: 20,
+                                  backgroundImage: NetworkImage(m["logo"]),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                InkWell(
+                                  child: MyTextWidget(
+                                    content: m["name"],
+                                    myColor: MyColor.blackText,
+                                    fontbold: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                  onTap: () {},
+                                ),
+                              ],
+                            )),
+                            DataCell(IconButton(
+                              icon: const Icon(Icons.highlight_remove_outlined),
+                              onPressed: () {},
+                            )),
+                          ],
+                        )
+                    ]),
+              ),
+            ])),
+      ),
+    ));
+  }
+
+  insideAllChannelsWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 100, bottom: 100),
+      child: SizedBox(
+        height: double.infinity,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: MyColor.whiteContainer,
+                  border: Border.all(color: MyColor.borderColor),
+                  borderRadius: BorderRadius.circular(5)),
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Column(children: [
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: DataTable(
+                          columnSpacing: (ResponsiveD.d(context) ==
+                                  ResponsiveD.mobile)
+                              ? 100
+                              : (ResponsiveD.d(context) == ResponsiveD.tablet)
+                                  ? 150
+                                  : 250,
+                          showCheckboxColumn: true,
+                          columns: [
+                            DataColumn(
+                                label: MyTextWidget(
+                                    content: "CHANNELS",
+                                    myColor: MyColor.greyText,
+                                    fontbold: FontWeight.bold)),
+                            DataColumn(
+                                label: MyTextWidget(
+                                    content: "ACTION",
+                                    myColor: MyColor.greyText,
+                                    fontbold: FontWeight.bold)),
+                          ],
+                          rows: [
+                            for (Map m in tabelList)
+                              DataRow(
+                                cells: [
+                                  DataCell(Row(
+                                    children: [
+                                      CircleAvatar(
+                                        radius: 20,
+                                        backgroundImage:
+                                            NetworkImage(m["logo"]),
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      InkWell(
+                                        child: MyTextWidget(
+                                          content: m["name"],
+                                          myColor: MyColor.blackText,
+                                          fontbold: FontWeight.bold,
+                                          fontSize: 14,
+                                        ),
+                                        onTap: () {},
+                                      ),
+                                    ],
+                                  )),
+                                  DataCell(IconButton(
+                                    icon: const Icon(Icons.add_box_outlined),
+                                    onPressed: () {},
+                                  )),
+                                ],
+                              )
+                          ]),
                     ),
-                  ],
-                ),
-              )
-            ]),
+                  ])),
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+List<Map<String, dynamic>> tabelList = [
+  {
+    "name": "Sarkari Job",
+    "isVerified": true,
+    "logo": "https://picsum.photos/210/300",
+    "totalMembers": "5,00,000",
+    "intalledPlugin": "e-Mitra & CSC",
+    "morePlugin": 5,
+    "admin": "Harphool",
+    "moreAdmin": 3,
+    "type": "Public"
+  },
+  {
+    "name": "Jigyasa Institute",
+    "isVerified": false,
+    "logo": "https://picsum.photos/220/300",
+    "totalMembers": "90,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 1,
+    "admin": "Ranu Meena",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "Agriculture Education Hub",
+    "isVerified": true,
+    "logo": "https://picsum.photos/240/300",
+    "totalMembers": "90,500",
+    "intalledPlugin": "Promotion",
+    "morePlugin": 4,
+    "admin": "Lokesh",
+    "moreAdmin": 5,
+    "type": "Private"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+  {
+    "name": "e-Mitra",
+    "isVerified": true,
+    "logo": "https://picsum.photos/250/300",
+    "totalMembers": "5,000",
+    "intalledPlugin": "Doctor",
+    "morePlugin": 5,
+    "admin": "Satish Sir",
+    "moreAdmin": 2,
+    "type": "Public"
+  },
+];
