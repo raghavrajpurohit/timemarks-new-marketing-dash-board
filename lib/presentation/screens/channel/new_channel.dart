bbc2c4f638119292dc3c0075562af00939ae7a5d
Raghav Rpurohit <raghavrajpurohit81@gmail.com>
2023-09-19 20:19:49 +0530

    chal araha hai kaam

--- lib/presentation/screens/channel/new_channel.dart
+++ lib/presentation/screens/channel/new_channel.dart
@@ -5,6 +5,8 @@ import 'package:new_marketing_dash_board/constants/responsive.dart';
 import 'package:new_marketing_dash_board/data/network_helper/html_js.dart';
 
 import 'package:new_marketing_dash_board/presentation/screens/channel/channel.dart.dart';
+import 'package:new_marketing_dash_board/presentation/screens/hirring/hirring.dart';
+import 'package:new_marketing_dash_board/presentation/screens/home_page.dart';
 import 'package:new_marketing_dash_board/presentation/widgets/myTextWidget.dart';
 import 'package:new_marketing_dash_board/constants/my_flutter_app_icons.dart';
 
@@ -25,6 +27,7 @@ class _AddChannelState extends State<AddChannel> {
   late int _selectedIndex;
   List<Widget> allPages = [
     const AddNewChannel(),
+    const HirringScreen(),
   ];
 
   @override
@@ -120,10 +123,71 @@ class AddNewChannel extends StatefulWidget {
 class _AddNewChannelState extends State<AddNewChannel> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: 300,
-      decoration: BoxDecoration(color: MyColor.appBarColor),
+    return Padding(
+      padding: const EdgeInsets.only(top: 25, left: 30, right: 30, bottom: 8),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              MyTextWidget(
+                content: "New Channel",
+                myColor: MyColor.blackText,
+                fontSize: 20,
+                fontbold: FontWeight.bold,
+              ),
+              SizedBox(
+                height: 40,
+                width: 134,
+                child: FilledButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(const Color(0xff262338))),
+                    onPressed: () {},
+                    child: MyTextWidget(
+                      content: "Create",
+                      myColor: MyColor.whiteContainer,
+                    )),
+              )
+            ],
+          ),
+          Row(
+            // spacing: 25,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Material(
+                    elevation: 4,
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 35, left: 8, right: 8, bottom: 8),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Column(children: [
+                            const CircleAvatar(
+                              radius: 20,
+                              backgroundImage:
+                                  NetworkImage("https://picsum.photos/200/300"),
+                            ),
+                            MyTextWidget(
+                              content: "Upload",
+                              myColor: MyColor.blueTextColor,
+                            ),
+                            const MyTextWidget(
+                                content:
+                                    "It’s recommended to use a picture that’s at least 98 x 98 pixels and 4MB or less. Use a PNG or GIF (no animations) file.")
+                          ]),
+                        ),
+                      ),
+                    ),
+                  )),
+              Expanded(flex: 8, child: Container()),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
